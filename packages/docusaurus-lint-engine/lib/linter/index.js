b8e7030f8c03c22261f64360af2eee74f2df1f7b
fanny <fanny.vieira@ccc.ufcg.edu.br>
2020-06-09 19:36:05 -0300

    feat(v2): start the linter api

--- packages/docusaurus-lint-engine/lib/linter/index.js
+++ packages/docusaurus-lint-engine/lib/linter/index.js
@@ -4,5 +4,17 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+const md = require('markdown-it')();
+const fs = require('fs');
 
-module.exports = {}
+// webpack alias, es6 import and export
+const rules = require('../rules')
+const content = fs.readFileSync('../tests/test-01.md', 'utf-8');
+
+const tokens = md.parse(content);
+// formatter
+const results = rules.map(rule => {
+  return rule.create({tokens})
+})
+
+console.log(results)
